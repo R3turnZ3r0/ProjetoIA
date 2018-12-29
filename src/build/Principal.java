2c965c21eb63765584700aa36b96ed2b3a33b27b
R3turnZ3r0 <netogiuseppe@hotmail.com>
2018-12-28 21:27:04 -0300

    Ha erros

--- src/build/Principal.java
+++ src/build/Principal.java
@@ -8,46 +8,61 @@ public class Principal {
     public static void main(String[] args)
     {
         double error = 0.0;
-        int npatt = 36;
-
-
-
-
-        int[] layers = new int[]{ 9, 9, 9 };
+        double maxit = 286;
+        int[] layers = new int[]{10,9,8,7,6,5,4,3,2,1 };
 
         MultiLayerPerceptron net = new MultiLayerPerceptron(layers, 0.6, new SigmoidalTransfer());
 
         String pattern = "/home/giuseppe/Downloads/2018.2/IA/Projeto/project/src/build/breast-cancer.arff"; //cortar para ter teste e training
 
         try {
-            ArffProcessing.loadArff(pattern,9);
+
+            ArffProcessing a = new ArffProcessing();
+            a.loadArff(pattern);
+            double[][] inputs = a.getInput();
+            double[][] output = a.getOutput();
+            int i = 0;
+
+            while((i < maxit))
+            {
+                if(inputs[i] == null)
+                {
+                    i++;
+                    continue;
+                }
+
+                if(output[i] == null)
+                {
+                    i++;
+                    continue;
+                }
+
+                // Training
+                error = net.backPropagate(inputs[i], output[i]);
+                System.out.println("Error at step "+i+" is "+error);
+
+                i++;
+            }
+
+            System.out.println("Learning completed!");
+
+            /* Test */
+            double [] inputT = {4.0,
+                    1.0,
+                    8.0,
+                    0.0,
+                    1.0,
+                    2.0,
+                    0.0,
+                    2.0,
+                    1.0,
+                    0.0};
+            double [] outputT=net.execute(inputT);
+
+            System.out.println("RESP "+output[0][0]);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //double[] output = new double[npatt]; //COLOCAR OS OUTPUTS DO PATTERNR ESTRUTURADO AQ
-  //      double[] inputT = {1,2,3}; // COLOCAR O PATTERN ESTRUTURADO AQ
-        //double[] outputT = new double[npatt]; //COLOCAR OS OUTPUTS DO PATTERNR ESTRUTURADO AQ
-    /*
-        for(int j = 1; j < npatt+1; j++)
-        {
-
-            if(inputs == null || output == null)
-            {
-                System.out.println("Cant find "+pattern);
-                continue;
-            }
-
-            // Training
-            error = net.backPropagate(inputs, output);
-            System.out.println("Error is "+error);
-        }
-
-        System.out.println("Learning completed!");
-*/
-        /* Test */
-//        outputT=net.execute(inputT);
-
-//        System.out.println(output);
     }
 
 }
